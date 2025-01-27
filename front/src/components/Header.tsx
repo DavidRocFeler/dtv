20f75309d09607c68c6aa7a7a71e76793b4c1507
Carlos Gonzalez <carloseliogonzalezapa1997@gmail.com>
2025-01-27 06:03:41 -0500

    Advance Header 2

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,67 +1,31 @@
 "use client"; // Marca este componente como un componente del lado del cliente
 
-import React, { useState, useEffect } from 'react'; // Importa useEffect
-import Image from 'next/image';
+import React, { useState } from 'react'; // Importa useEffect
 import ButtonHeader from './ButtonHeader';
 import DropDown from './DropDown';
 import SearchBar from './SearchBar';
 import SectionSwitcher from './SectionSwitcher';
-import styles from '../styles/ButtonHeader.module.css';
 import Link from 'next/link'; // Importa Link si no lo tienes
 
 const Header: React.FC = () => {
   const [isLoggerIn, setIsLoggedIn] = useState(false);
-  const [isHydrated, setIsHydrated] = useState(false); // Estado para controlar la hidratación
-
-  // Efecto para marcar el componente como hidratado
-  useEffect(() => {
-    setIsHydrated(true);
-  }, []);
 
   const handlelogin = () => {
     setIsLoggedIn(true);
   };
 
-  // Si el componente no está hidratado, retorna la estructura básica
-  if (!isHydrated) {
-    return (
-      <header className="bg-black text-white py-4">
-        <div className="px-4 flex flex-row items-center justify-between">
-          {/* Logo */}
-          <div className="flex-shrink-0 ml-4">
-            <Link href="/">
-              <Image
-                src="/logo.png" // Asegúrate de que esta ruta sea correcta
-                alt="Logo"
-                width={60} // Define un ancho fijo
-                height={60} // Define un alto fijo
-                className="Logo"
-              />
-            </Link>
-          </div>
-
-          {/* Botones de inicio de sesión y registro (no hidratado) */}
-          <div className="flex flex-row items-center space-x-4 mr-4">
-            <button className={`${styles.ButtonGradient}`}>Login in</button>
-            <button className={`${styles.ButtonGradient}`}>Sign up</button>
-          </div>
-        </div>
-      </header>
-    );
-  }
 
   // Si el componente está hidratado, retorna la estructura completa
   return (
-    <header className="bg-black text-white py-4">
+    <header className="bg-black text-white py-[0.6rem]">
       <div className="px-4 flex flex-row items-center justify-between">
         {/* Logo */}
-        <div className="flex-shrink-0 ml-4">
+        <div className="flex-shrink-0 ml-[0rem]">
           <Link href="/">
-            <Image
+            <img
               src="/logo.png"
               alt="Logo"
-              width={60}
-              height={60}
+              className='w-[60px] h-auto'
             />
           </Link>
         </div>
@@ -88,7 +52,9 @@ const Header: React.FC = () => {
             </div>
 
             {/* SearchBar y DropDown */}
-            <div className="flex flex-row items-center space-x-4 ml-auto mr-[2rem]">
+            <div className="flex flex-row items-center space-x-4 ml-auto mr-[0] 
+              sm:mr-[2rem] 
+            bg-red-500">
               <SearchBar />
               <DropDown />
             </div>
@@ -99,4 +65,4 @@ const Header: React.FC = () => {
   );
 };
 
-export default Header;
+export default Header;
