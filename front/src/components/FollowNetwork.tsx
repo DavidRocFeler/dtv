f7a769547093efa2cfa1439ad25903c672725a45
Carlos Gonzalez <carloseliogonzalezapa1997@gmail.com>
2025-02-01 08:08:26 -0500

    Advance Footer

---
+++ front/src/components/FollowNetwork.tsx
@@ -0,0 +1,21 @@
+
+const FollowNetwork = () => {
+  return (
+    <div className="flex flex-col items-start py-3 gap-4 md:gap-6 max-w-4xl  ml-[2rem]">
+        <div className="tittle_follow">
+            <h2>Follow</h2>
+        </div>
+        <div className="flex flex-row items-center gap-5 social_media_icons py-3">
+            <img src="/FacebookLogo.svg"/>
+            <img src="/InstagramLogo.svg"/>
+            <img src="/TiktokLogo.svg"/>
+            <img src="/XLogo.svg"/>
+        </div>
+        <div className="text-[#414141]">
+            2025 Stream-d | developed by Start4
+        </div>
+    </div>
+  )
+}
+
+export default FollowNetwork
