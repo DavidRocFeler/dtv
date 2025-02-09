44e693e9746106eec7f0aed6b429a30f419fcf2f
David Natan <rdpalominop@ucvvirtual.edu.pe>
2025-02-09 11:05:06 +0100

    new css modules by deploy

--- front/next.config.ts
+++ front/next.config.ts
@@ -4,7 +4,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  output: "standalone", 
+  output: "standalone",
+  cssModules: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
